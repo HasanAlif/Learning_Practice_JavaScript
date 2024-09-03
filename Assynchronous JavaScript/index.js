5c02b2b3edfe1bfd4e6f3c90e9d74871a8106079
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2024-09-03 23:47:34 +0600

    commit

--- Assynchronous JavaScript/index.js
+++ Assynchronous JavaScript/index.js
@@ -32,3 +32,81 @@ const welcome2 = function(){
 setTimeout(welcome2,2000);
 
 console.log("C");
+
+
+
+
+
+
+console.log("A");
+
+const logInterval = setInterval(()=>{
+    console.log("Interval");
+},1000);
+clearInterval(logInterval);
+
+console.log("C");
+
+
+
+
+console.log("Start");
+
+function loginUser(email,password,callback){
+    setTimeout(()=>{
+        userData = {userId: 592 , status: "success"};
+        callback(userData);
+    },2000);
+}
+
+const data = loginUser("alifalmehedihasan@gmail.com","random",(values)=> console.log(values));
+
+console.log("Finish");
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Start");
+
+function loginUser(email,password,callback){
+    setTimeout(()=>{
+        userData = {userId: 592 , status: "success"};
+        console.log("Inside loginUser");
+        callback(userData);
+    },2000);
+}
+
+function getUserTweets(userId,callback){
+    setTimeout(()=>{
+        tweetData = [{tweetId:1,content:"ABC"},{tweetId:2,content:"XYZ"}];
+        console.log("Inside getUserTweets");
+        callback(tweetData);
+    },1000); 
+}
+
+function getTweetComments(tweetId,callback){
+    setTimeout(()=>{
+        commentData = [{commentId: 1, content: "Awesome!"}];
+        console.log("Inside getTweetComments");
+        callback(commentData);
+    },1000);
+}
+
+const data2 = loginUser("alifalmehedihasan@gmail.com","random",(userData)=> {
+    getUserTweets(userData.userId,()=>{
+        getTweetComments(tweetData[0].tweetId,(commentData)=>{
+            console.log(commentData);
+        });
+    });
+});
+
+console.log("Finish");
+
